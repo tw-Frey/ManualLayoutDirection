2efde474557f5eafe7103abdefdb82a3bf0dc6f8
FY <tw-Frey@users.noreply.github.com>
2021-09-10 07:16:40 +0800

    check isLayoutDirectionResolved
    Signed-off-by: FY <tw-Frey@users.noreply.github.com>

--- app/src/main/java/tw/idv/fy/manual/layoutdirection/MainActivity.kt
+++ app/src/main/java/tw/idv/fy/manual/layoutdirection/MainActivity.kt
@@ -4,8 +4,9 @@ import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.view.View.LAYOUT_DIRECTION_LOCALE
+import android.view.View.*
 import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -18,9 +19,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d("Faty", "LAYOUT_DIRECTION_LTR     = $LAYOUT_DIRECTION_LTR")
+        Log.d("Faty", "LAYOUT_DIRECTION_RTL     = $LAYOUT_DIRECTION_RTL")
+        Log.d("Faty", "LAYOUT_DIRECTION_INHERIT = $LAYOUT_DIRECTION_INHERIT")
+        Log.d("Faty", "LAYOUT_DIRECTION_LOCALE  = $LAYOUT_DIRECTION_LOCALE")
+
         val item0 = findViewById<FrameLayout>(R.id.view0)[0]
         val view1 = findViewById<ImageView>(R.id.view1)
         val view2 = findViewById<ImageView>(R.id.view2)
+
+        item0.post {
+            Log.v("Faty", "(InLaid) layoutDirection = ${item0.layoutDirection}")
+            Log.v("Faty", "(InLaid) isLayoutDirectionResolved = ${item0.isLayoutDirectionResolved}")
+            Log.v("Faty", "(InLaid) canResolveLayoutDirection = ${item0.canResolveLayoutDirection()}")
+        }
 
         view1.post {
 
@@ -47,7 +59,13 @@ class MainActivity : AppCompatActivity() {
             val heightSpec = View.MeasureSpec.makeMeasureSpec(item0.height, View.MeasureSpec.EXACTLY)
 
             val item2 = layoutInflater.inflate(R.layout.item_tv, null)
+            Log.i("Faty", "(before) layoutDirection = ${item2.layoutDirection}")
+            Log.i("Faty", "(before) isLayoutDirectionResolved = ${item2.isLayoutDirectionResolved}")
+            Log.i("Faty", "(before) canResolveLayoutDirection = ${item2.canResolveLayoutDirection()}")
             item2.layoutDirection = LAYOUT_DIRECTION_LOCALE
+            Log.i("Faty", "(after)  layoutDirection = ${item2.layoutDirection}")
+            Log.i("Faty", "(after)  isLayoutDirectionResolved = ${item2.isLayoutDirectionResolved}")
+            Log.i("Faty", "(after)  canResolveLayoutDirection = ${item2.canResolveLayoutDirection()}")
             item2.measure(widthSpec, heightSpec)
             item2.layout(item0.left, item0.top, item0.right, item0.bottom)
 
